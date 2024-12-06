43fe450358982bf06b5ccd09797a40ee1818044c
mutiarahns <mutiarahns@gmail.com>
2024-12-06 17:03:23 +0700

    new design for address-book

---
+++ index.js
@@ -0,0 +1,46 @@
+const contacts = [
+  {
+    id: 1,
+    name: "Mutiara Nofitasari",
+    email: "mutiara@aaa.com",
+    phone: "+62 888-888-8888",
+    address: "123 Main St, New York, NY 10001",
+    status: "Approved",
+    idFavorite: true,
+    isDeleted: false,
+    createdDate: "6/10/2024",
+    deletedDate: null,
+    labels: [2],
+  },
+  {
+    id: 2,
+    name: "Sahbana",
+    email: "mutiara@aaa.com",
+    phone: "+62 888-888-8888",
+    address: "123 Main St, New York, NY 10001",
+    status: "Approved",
+    idFavorite: false,
+    isDeleted: false,
+    createdDate: "6/10/2024",
+    deletedDate: null,
+    labels: [1, 2],
+  },
+];
+
+const labels = [
+  {
+    labelId: 1,
+    labelName: "Work",
+    color: "#00C853",
+  },
+  {
+    labelId: 2,
+    labelName: "Family",
+    color: "#00C853",
+  },
+  {
+    labelId: 3,
+    labelName: "Friends",
+    color: "#00C853",
+  },
+];
